02609cd9e86adc8215792ffcb64c6dad81e51388
Riccardo Massari <maxdrift85@gmail.com>
2022-04-10 01:14:56 +0200

    Fix Seeds' priv dir path

--- lib/safarimanager/seeds.ex
+++ lib/safarimanager/seeds.ex
@@ -2,19 +2,22 @@ NimbleCSV.define(SM.CSV, separator: ";")
 
 defmodule SM.Seeds do
   alias SM.CSV, as: CSV
-  # alias SM.Evaluations
+  alias SM.Evaluations
   alias SM.Subjects
 
   @spec run :: :ok
   def run do
     insert_subjects()
-    # insert_evaluations()
+    insert_evaluations()
   end
 
   # Internal
 
   defp insert_subjects do
-    "priv/repo/elenco_pesci_2019.csv"
+    priv_dir = :code.priv_dir(:safarimanager)
+
+    [priv_dir, "/repo/elenco_pesci_2019.csv"]
+    |> Path.join()
     |> File.stream!()
     |> CSV.parse_stream(skip_headers: false)
     |> Stream.map(fn [numeric_id, name, scientific_name, coefficient] ->
@@ -30,15 +33,15 @@ defmodule SM.Seeds do
     |> Stream.run()
   end
 
-  # defp insert_evaluations do
-  #   0..10
-  #   |> Stream.map(fn e ->
-  #     {:ok, _result} =
-  #       Evaluations.create(%{
-  #         value: Decimal.new(e),
-  #         type: "numeric"
-  #       })
-  #   end)
-  #   |> Stream.run()
-  # end
+  defp insert_evaluations do
+    0..10
+    |> Stream.map(fn e ->
+      {:ok, _result} =
+        Evaluations.create(%{
+          value: Decimal.new(e),
+          type: "numeric"
+        })
+    end)
+    |> Stream.run()
+  end
 end
