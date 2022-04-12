3cd1eae02630c082a5affead5dcd41adce7a3cdc
Riccardo Massari <maxdrift85@gmail.com>
2022-04-12 02:30:17 +0200

    Comment out Search Select component

--- lib/safarimanager_web/components/search_select_test/search_select.ex
+++ lib/safarimanager_web/components/search_select_test/search_select.ex
@@ -1,14 +1,14 @@
-defmodule SMWeb.Components.SearchSelect do
-  @moduledoc """
-  Search select component.
-  """
-  use Surface.Components.Form.Input
+# defmodule SMWeb.Components.SearchSelect do
+#   @moduledoc """
+#   Search select component.
+#   """
+#   use Surface.Components.Form.Input
 
-  import Phoenix.HTML.Form, only: [input_id: 2]
+#   import Phoenix.HTML.Form, only: [input_id: 2]
 
-  prop items, :list, default: []
-  prop search, :event, required: true
-  prop select_item, :event, required: true
-  prop selected_label, :string, default: nil
-  prop phx_debounce, :integer, default: 500
-end
+#   prop items, :list, default: []
+#   prop search, :event, required: true
+#   prop select_item, :event, required: true
+#   prop selected_label, :string, default: nil
+#   prop phx_debounce, :integer, default: 500
+# end
