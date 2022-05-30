fa901dc5a4b228c1c0223a2de4e3aa59dd0dede6
Riccardo Massari <maxdrift85@gmail.com>
2022-05-30 15:41:29 +0200

    Make Competition steps clickable with links

--- lib/safarimanager_web/components/steps_header.ex
+++ lib/safarimanager_web/components/steps_header.ex
@@ -4,17 +4,20 @@ defmodule SMWeb.Components.StepsHeader do
   """
   use SMWeb, :surface_component
 
+  prop competition_id, :string, required: true
   prop current_step, :integer, required: true
 
-  defp steps do
+  @root_url "/organize"
+
+  defp steps(competition_id) do
     [
-      %{name: "Participants"},
-      %{name: "Jurors"},
-      %{name: "Slides"},
-      %{name: "CSV"},
-      %{name: "Validation"},
-      %{name: "Jury"},
-      %{name: "Results"}
+      %{name: "Participants", url: Path.join([@root_url, competition_id, "/participants"])},
+      %{name: "Jurors", url: Path.join([@root_url, competition_id, "/jurors"])},
+      %{name: "Slides", url: Path.join([@root_url, competition_id, "/slides"])},
+      %{name: "CSV", url: Path.join([@root_url, competition_id, "/csv_import"])},
+      %{name: "Validation", url: Path.join([@root_url, competition_id, "/validation_launcher"])},
+      %{name: "Jury", url: Path.join([@root_url, competition_id, "/jury_launcher"])},
+      %{name: "Results", url: Path.join([@root_url, competition_id, "/results"])}
     ]
   end
 end
