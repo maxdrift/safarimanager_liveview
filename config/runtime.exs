8a6c99867b4a52a3c7cc632d06b756badd0afb55
Riccardo Massari <maxdrift85@gmail.com>
2022-10-03 00:48:16 +0200

    Consolidate config

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,11 +7,6 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
-# Start the phoenix server if environment is set and running in a  release
-if System.get_env("PHX_SERVER") && System.get_env("RELEASE_NAME") do
-  config :safarimanager, SMWeb.Endpoint, server: true
-end
-
 priv_dir = :code.priv_dir(:safarimanager) |> to_string()
 
 priv_dir_uploads = Path.join(priv_dir, "/uploads")
@@ -36,107 +31,69 @@ end
 
 config :safarimanager, SM.Repo, database: db_path
 
-if config_env() == :standalone do
-  secret_key_base = "MROe0TML1wb/8amRSKJ994MKvIpq3JqLM/FMevNTM2YSEst8NXNkcTSkV/C17DP0"
+config :logger, level: String.to_atom(System.get_env("LOG_LEVEL", "info"))
 
-  config :safarimanager, SMWeb.Endpoint,
-    http: [
-      port: String.to_integer(System.get_env("PORT") || "4000"),
-      transport_options: [socket_opts: [:inet6]]
-    ],
-    secret_key_base: secret_key_base
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
-  # os_name =
-  #   case :os.type() do
-  #     {:unix, :darwin} -> :darwin
-  #     {:unix, :linux} -> :linux
-  #     {:win32, :nt} -> :windows
-  #   end
+lv_signing_salt =
+  System.get_env("LV_SIGNING_SALT") ||
+    raise """
+    environment variable LV_SIGNING_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret 32
+    """
 
-  # cache_dir = :filename.basedir(:user_cache, "safarimanager")
-  # config_dir = :filename.basedir(:user_data, "safarimanager")
-  data_dir = :filename.basedir(:user_data, "safarimanager")
+url_host = System.get_env("URL_HOST", "localhost")
+url_port = "URL_PORT" |> System.get_env("443") |> String.to_integer()
 
-  IO.puts("User directory: #{data_dir}")
-end
+http_ip = {0, 0, 0, 0, 0, 0, 0, 0}
+http_port = "HTTP_PORT" |> System.get_env("4000") |> String.to_integer()
 
-if config_env() == :prod do
-  # database_url =
-  #   System.get_env("DATABASE_URL") ||
-  #     raise """
-  #     environment variable DATABASE_URL is missing.
-  #     For example: ecto://USER:PASS@HOST/DATABASE
-  #     """
+allowed_origins =
+  "ALLOWED_ORIGINS"
+  |> System.get_env("")
+  |> String.split(",", trim: true)
 
-  # maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+config :safarimanager, SMWeb.Endpoint,
+  url: [host: url_host, port: 443],
+  http: [
+    # Enable IPv6 and bind on all interfaces.
+    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    ip: http_ip,
+    port: http_port
+  ],
+  check_origin: [
+    "http://#{url_host}:#{url_port}",
+    "http://#{url_host}:#{http_port}" | allowed_origins
+  ],
+  secret_key_base: secret_key_base,
+  live_view: [signing_salt: lv_signing_salt]
 
-  # config :safarimanager, SM.Repo,
-  #   # ssl: true,
-  #   url: database_url,
-  #   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  #   socket_options: maybe_ipv6
+# ## Configuring the mailer
+#
+# In production you need to configure the mailer to use a different adapter.
+# Also, you may need to configure the Swoosh API client of your choice if you
+# are not using SMTP. Here is an example of the configuration:
+#
+#     config :safarimanager, SM.Mailer,
+#       adapter: Swoosh.Adapters.Mailgun,
+#       api_key: System.get_env("MAILGUN_API_KEY"),
+#       domain: System.get_env("MAILGUN_DOMAIN")
+#
+# For this example you need include a HTTP client required by Swoosh API client.
+# Swoosh supports Hackney and Finch out of the box:
+#
+#     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
+#
+# See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
-  allowed_origins =
-    "ALLOWED_ORIGINS"
-    |> System.get_env("")
-    |> String.split(",", trim: true)
-
-  config :safarimanager, SMWeb.Endpoint,
-    url: [host: host, port: 443],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
-    check_origin: ["http://localhost:4000" | allowed_origins],
-    secret_key_base: secret_key_base
-
-  # ## Using releases
-  #
-  # If you are doing OTP releases, you need to instruct Phoenix
-  # to start each relevant endpoint:
-  #
-  #     config :safarimanager, SMWeb.Endpoint, server: true
-  #
-  # Then you can assemble a release by calling `mix release`.
-  # See `mix help release` for more information.
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :safarimanager, SM.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
-end
+# end
 
 {:ok, hostname} = :inet.gethostname()
 
