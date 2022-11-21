599e58730a0073e34ccbe28c77da4aeb75fd2839
Riccardo Massari <maxdrift85@gmail.com>
2022-11-21 17:09:54 +0100

    Replace Mogrify with Image (Vix)

--- lib/safarimanager/image_processing.ex
+++ lib/safarimanager/image_processing.ex
@@ -2,26 +2,21 @@ defmodule SM.ImageProcessing do
   @moduledoc """
   Image processing helpers
   """
-  import Mogrify
 
   @spec save_thumbnail(String.t(), non_neg_integer(), non_neg_integer(), String.t()) ::
-          {:ok, String.t()} | {:error, any()}
+          :ok | {:error, any()}
   def save_thumbnail(source, height, width, path) do
-    source
-    |> open()
-    |> resize_to_limit("#{height}x#{width}")
-    |> save(path: path)
-  rescue
-    File.Error -> {:error, :invalid_src_path}
-    error -> {:error, error}
-  else
-    _image -> {:ok, path}
+    # Consider calling put_concurrency/1 to avoid CPU starvation on RPi.
+    # Image.put_concurrency(concurrency)
+    with {:ok, image} <- Image.thumbnail(source, "#{height}x#{width}"),
+         {:ok, _image} <- Image.write(image, path),
+         do: :ok
   end
 
-  @spec get_info(String.t()) :: %{atom() => any()}
-  def get_info(path) do
-    identify(path)
-  end
+  # @spec get_info(String.t()) :: %{atom() => any()}
+  # def get_info(path) do
+  #   identify(path)
+  # end
 
   @spec get_metadata(String.t()) :: {:ok, %{(atom() | String.t()) => any()}} | {:error, any()}
   def get_metadata(path) do
