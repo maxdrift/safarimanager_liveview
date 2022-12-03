25288f0e7090cf0a48c86b032bf93f5d9914b512
Riccardo Massari <maxdrift85@gmail.com>
2022-12-03 23:44:02 +0100

    Attempt to fetch yarn deps during build

--- .github/scripts/app/build_mac.sh
+++ .github/scripts/app/build_mac.sh
@@ -12,5 +12,5 @@ set -e
 mix local.hex --force --if-missing
 mix local.rebar --force --if-missing
 MIX_ENV=prod MIX_TARGET=app mix deps.get --only prod
-MIX_ENV=prod MIX_TARGET=app mix assets.deploy
+MIX_ENV=prod MIX_TARGET=app yarn --cwd assets
 MIX_ENV=prod MIX_TARGET=app mix release app --overwrite
