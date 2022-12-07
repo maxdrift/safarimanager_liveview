4f108a201967ab75ea7696332546cb7c53472079
Riccardo Massari <maxdrift85@gmail.com>
2022-12-08 00:07:16 +0100

    Try to compile for MacOS 10.x

--- elixirkit/lib/elixirkit/nif.ex
+++ elixirkit/lib/elixirkit/nif.ex
@@ -5,8 +5,8 @@ defmodule ElixirKit.NIF do
   def __init__ do
     cc_target =
       case :erlang.system_info(:system_architecture) do
-        ~c"aarch64-apple-darwin" ++ _ -> "arm64-apple-macos11.0"
-        ~c"x86_64-apple-darwin" ++ _ -> "x86_64-apple-macos11.0"
+        ~c"aarch64-apple-darwin" ++ _ -> "arm64-apple-macos10.0"
+        ~c"x86_64-apple-darwin" ++ _ -> "x86_64-apple-macos10.0"
       end
 
     :erlang.load_nif(Application.app_dir(:elixirkit, "priv/#{cc_target}/elixirkit_nif"), 0)
