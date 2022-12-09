7a64267573cfae32a0bdcf59f25602008ac32f8c
Riccardo Massari <maxdrift85@gmail.com>
2022-12-09 16:06:02 +0100

    Set number of Vips schedulers

--- lib/safarimanager/image_processing.ex
+++ lib/safarimanager/image_processing.ex
@@ -6,9 +6,11 @@ defmodule SM.ImageProcessing do
   @spec save_thumbnail(String.t(), non_neg_integer(), non_neg_integer(), String.t()) ::
           :ok | {:error, any()}
   def save_thumbnail(source, height, width, path) do
-    # Consider calling put_concurrency/1 to avoid CPU starvation on RPi.
-    # Image.put_concurrency(concurrency)
-    with {:ok, image} <- Image.thumbnail(source, "#{height}x#{width}"),
+    System.schedulers_online()
+    |> determine_concurrency()
+    |> Image.put_concurrency()
+
+    with {:ok, image} <- Image.thumbnail(source, "#{height}x#{width}", crop: :attention),
          {:ok, _image} <- Image.write(image, path),
          do: :ok
   end
@@ -21,5 +23,17 @@ defmodule SM.ImageProcessing do
   @spec get_metadata(String.t()) :: {:ok, %{(atom() | String.t()) => any()}} | {:error, any()}
   def get_metadata(path) do
     Exexif.exif_from_jpeg_file(path)
+  end
+
+  defp determine_concurrency(1) do
+    1
+  end
+
+  defp determine_concurrency(number_of_schedulers) when rem(number_of_schedulers, 2) == 0 do
+    number_of_schedulers / 2
+  end
+
+  defp determine_concurrency(number_of_schedulers) do
+    determine_concurrency(number_of_schedulers - 1)
   end
 end
