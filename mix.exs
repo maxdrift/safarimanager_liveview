81de8cee7b771183bc4e037b2525284143f193ab
Riccardo Massari <maxdrift85@gmail.com>
2023-07-14 16:37:06 +0200

    Bump app version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SM.MixProject do
   use Mix.Project
 
   @elixir_requirement "~> 1.14"
-  @version "2023.7.2"
+  @version "2023.7.3"
   @description ~s(Application to manage "Underwater Photo Safari" competitions)
 
   @app_elixir_version "1.15.1"
