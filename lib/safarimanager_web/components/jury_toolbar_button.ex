207ebc065c0cc944971863dbf2aefd23a5349e0e
Riccardo Massari <maxdrift85@gmail.com>
2026-09-14 10:56:37 +0200

    Fix jury voting, penalties, and launcher layout
    Repair evaluation button phx-value attrs, base penalty quorum on enrolled
    jurors, clear votes on manual penalty with UI feedback, and use a
    responsive jury launcher grid. Add jury LiveView regression tests.

--- lib/safarimanager_web/components/jury_toolbar_button.ex
+++ lib/safarimanager_web/components/jury_toolbar_button.ex
@@ -13,23 +13,26 @@ defmodule SMWeb.Components.JuryToolbarButton do
 
   slot :inner_block
 
-  def jury_toolbar_button(%{click_key: _key} = assigns) do
+  def jury_toolbar_button(%{click: click} = assigns) when is_binary(click) do
+    phx_value_attrs =
+      case assigns do
+        %{click_key: key, click_value: value} when is_binary(key) and not is_nil(value) ->
+          %{"phx-value-#{key}" => value}
+
+        _ ->
+          %{}
+      end
+
+    assigns = assign(assigns, :phx_value_attrs, phx_value_attrs)
+
     ~H"""
     <button
       id={@id}
       phx-click={@click}
-      phx-value-{@click_key}={@click_value}
+      {@phx_value_attrs}
       class={@class}
       title={@title}
     >
-      {render_slot(@inner_block)}
-    </button>
-    """
-  end
-
-  def jury_toolbar_button(%{click: _click} = assigns) do
-    ~H"""
-    <button id={@id} phx-click={@click} class={@class} title={@title}>
       {render_slot(@inner_block)}
     </button>
     """
