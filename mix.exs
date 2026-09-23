35cb8a6331994e2262a1291f56680261245de47a
Riccardo Massari <maxdrift85@gmail.com>
2026-09-23 13:25:09 +0200

    Bump version to 26.9.7

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule SM.MixProject do
   use Mix.Project
 
   @elixir_requirement "~> 1.19"
-  @version "26.9.6"
+  @version "26.9.7"
   @description ~s(Application to manage "Underwater Photo Safari" competitions)
 
   def project do
